79be7ad994625ace5c23635f2f3bb77f8590f485
tahoang <dhoang519@gmail.com>
2015-03-19 12:36:53 -0400

    Minor fixes

--- data/dataRepository.js
+++ data/dataRepository.js
@@ -14,7 +14,8 @@ var SqlRepository = require('./sqlRepository');
 var Connection = require('tedious').Connection;
 var Request = require('tedious').Request;
 var TYPES = require('tedious').TYPES;
-var dbConfig = require('../../appSettings').dbConfig;
+var appPath = require('app-root-path');
+var dbConfig = require(appPath + '/appSettings').dbConfig;
 
 var DataRepository = Class.define({
   extend: SqlRepository,
