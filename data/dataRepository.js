255c7237ed9fae825f08418aa903c1b3d5d1075b
tahoang <dhoang519@gmail.com>
2015-12-01 11:40:16 -0500

    config folder for db is now renamed to dbconfig instead of config

--- data/dataRepository.js
+++ data/dataRepository.js
@@ -15,7 +15,7 @@ var Connection = require('tedious').Connection;
 var Request = require('tedious').Request;
 var TYPES = require('tedious').TYPES;
 var appPath = require('app-root-path');
-var dbConfig = require(appPath + '/config').dbConfig;
+var dbConfig = require(appPath + '/dbconfig').dbConfig;
 
 var DataRepository = Class.define({
   extend: SqlRepository,
